a059babf95a121e6b6851361d56681453ce62a9a
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-08-26 12:13:00 +0530

    model register

--- user_auth/models.py
+++ user_auth/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 
 
 class CustomUser(AbstractUser):
+    """
+    Custom User Model with email and mobile number as unique fields
+    and email and mobile number verification fields and otp fields.
+    """
     email = models.EmailField(unique=True)
     mobile_number = models.CharField(max_length=15, unique=True)
     is_email_verified = models.BooleanField(default=False)
